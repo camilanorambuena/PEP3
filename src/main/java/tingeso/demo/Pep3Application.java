aa08e0417944d139464c3a21793f157f55937474
Jorge sandoval <jorge.sandoval.m@usach.cl>
2021-01-31 00:40:45 -0300

    base proyecto

---
+++ src/main/java/tingeso/demo/Pep3Application.java
@@ -0,0 +1,14 @@
+package tingeso.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pep3Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hola humanos de Tingeso");
+		SpringApplication.run(Pep3Application.class, args);
+	}
+
+}
---
+++ src/test/java/tingeso/demo/Pep3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tingeso.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pep3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
